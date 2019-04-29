2812ec87f2d810d93af44697626c32f841bd96a4
Anton <antonsgrouva@gmail.com>
2019-04-29 22:02:29 +0300

    Securing Keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI:
-        'mongodb+srv://Dev:12345@devconnector-xhrgx.mongodb.net/test?retryWrites=true',
-    secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
